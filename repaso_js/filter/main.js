53460ac2066aa477372a0c2e9136d133c13c94f5
danaaguilar <danaaguilar2017@gmail.com>
2022-09-06 17:05:39 -0300

    subo actividad

--- repaso_js/filter/main.js
+++ repaso_js/filter/main.js
@@ -2,3 +2,12 @@ let helados = ['banana split','menta granizada','cereza','dulce de leche','grani
 
 let arreglo_filtrado = helados.filter(element => element.length>9)
 console.log(arreglo_filtrado)
+
+//ejemplo 2
+let edades = [12,5,8,130,44]
+function filtrar_por_edades(element){
+    return element >= 10 
+}
+let filtrado_edades = edades.filter(filtrar_por_edades)
+console.log(filtrado_edades)
+
